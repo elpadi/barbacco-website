78c7481e0d1b831b0cc1b68a81ed583bef060254
carlos (minimal) <padi_05@yahoo.com>
2018-03-25 15:54:04 -0400

    Finishing tweaks

--- wp-content/themes/barbacco/archive.php
+++ wp-content/themes/barbacco/archive.php
@@ -1,33 +1,44 @@
 <?php get_header();
-$featuredImage = wp_get_attachment_url( get_post_thumbnail_id( 48 ) ); ?>
-?><div class="barbacco-giant-background">
+$archive_settings_pages = [
+	'barbacco_news' => 673,
+];
+$archive_name = get_query_var('post_type');
+if (isset($archive_settings_pages[$archive_name])) $settingsPage = get_post($archive_settings_pages[$archive_name]);
+?>
+<?php if (isset($settingsPage)): ?>
+<div class="barbacco-giant-background">
 	</div><!-- end of barbacco-giant-background -->
 	<style>
-		.barbacco-giant-background { background:url('<?php echo $featuredImage; ?>') center center; background-size:cover; }
+		.barbacco-giant-background { background:url('<?php echo  wp_get_attachment_url( get_post_thumbnail_id( $settingsPage->ID ) ); ?>') center center; background-size:cover; }
 	</style>
 </div>		<!-- end of headerWrapper -->
 <div class="clear"></div>
+<?php endif; ?>
 <div id="section-post-index" class="barbacco-sec-content">
 	<div class="container-fluid">
 		<div class="row">
 			<div class="col-xs-12 barbacco-section-header">
-				<h1 class="barbacco-main-color"><?php echo str_replace('Archives: ', '', get_the_archive_title()); ?></h1>
+				<h1 class="barbacco-main-color"><?php echo isset($settingsPage) ? $settingsPage->post_title : str_replace('Archives: ', '', get_the_archive_title()); ?></h1>
 			</div><!-- end of barbacco-section-header -->
 			<div class="col-xs-12 barbacco-section-content barbacco-main-color">
 				<div class="barbacco-the-content">
 					<div class="the-submenu-page-content">
+						<?php if ( have_posts() && current_user_can('edit_post', 48) ) : ?>
 						<div class="the-submenu-content-box">
-							<?php if ( have_posts() || current_user_can('edit_post', 48) ) : while ( have_posts() ) : the_post(); for ($i = 0; $i < 12; $i++): ?>
+							<?php while ( have_posts() ) : the_post(); ?>
 							<article class="post">
 								<a href="<?php the_permalink(); ?>"><img src="<?php echo wp_get_attachment_url( get_post_thumbnail_id( $post->ID ) ); ?>" alt=""></a>
 								<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
 								<p><?php echo get_the_date(); ?></p>
 							</article>
-							<?php endfor; endwhile; else: ?>
+							<?php endwhile; ?>
+						</div><!-- end of the-submenu-content-box -->
+						<?php else: ?>
+						<div>
 							<p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentyseventeen' ); ?></p>
 							<?php get_search_form(); ?>
-							<?php endif; ?>
 						</div><!-- end of the-submenu-content-box -->
+						<?php endif; ?>
 					</div><!-- end of the-submenu-page-content -->
 				</div><!-- end of tab-content -->
 			</div><!-- end of barbacco-section-content -->
--- wp-content/themes/barbacco/single.php
+++ wp-content/themes/barbacco/single.php
@@ -1,35 +1,31 @@
-<?php get_header(); the_post(); ?>
-
-		<div id="section-<?php the_ID(); ?>" class="barbacco-sec-content">
-			<div class="container-fluid">
-				<div class="row">
-				
-					<div class="col-xs-12 barbacco-section-content barbacco-main-color">
-						<div class="barbacco-the-content">
-							
-							<div id="fd-<?php echo $post_slug; ?>" class="tab-pane fade in active">
-								<div class="the-submenu-page-title">
-									<h2 class="text-uppercase"><?php the_title(); ?></h2>
-									<h3 class="text-uppercase"><?php the_field('news_article_headline'); ?></h2>
-								</div><!-- end of the-submenu-page-title -->
-								<div class="the-submenu-page-content">
-									<div class="the-submenu-content-box">
-										<p><img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>" alt=""></p>
-										<?php while (have_rows('news_article_images')): the_row(); ?>
-										<p><img src="<?php echo get_sub_field('news_article_image')['url']; ?>" alt=""></p>
-										<p><?php echo get_sub_field('news_article_image')['alt']; ?></p>
-										<?php endwhile; ?>
-									</div><!-- end of the-submenu-content-box -->
-								</div><!-- end of the-submenu-page-title -->
-							</div><!-- end of tab-pane -->
-								
-						</div><!-- end of tab-content -->
-					</div><!-- end of barbacco-section-content -->
-							
-				</div><!-- end of row -->
-			</div><!-- end of container -->
-		</div><!-- end of barbacco-sec-content -->
-		<div class="clear"></div><!-- end of clear -->
-					
-<?php get_footer(); ?>
-<?php include(__DIR__.'/page.php');
+<?php get_header(); the_post();
+?><div id="section-<?php the_ID(); ?>" class="barbacco-sec-content">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-xs-12 barbacco-section-header">
+				<h1 class="barbacco-main-color"><?php the_title(); ?></h1>
+				<h2 class="barbacco-main-color"><?php the_field('news_article_headline'); ?></h2>
+			</div><!-- end of barbacco-section-header -->
+			<div class="col-xs-12 barbacco-section-content barbacco-main-color">
+				<div class="barbacco-the-content">
+					<div class="the-submenu-page-content">
+						<div class="the-submenu-content-box">
+							<figure>
+								<img src="<?php echo wp_get_attachment_url(get_post_thumbnail_id($post->ID)); ?>" alt="">
+								<figcaption><?php echo wp_get_attachment_caption(get_post_thumbnail_id($post->ID)); ?></figcaption>
+							</figure>
+							<?php while (have_rows('news_article_images')): the_row(); ?>
+							<figure>
+								<img src="<?php echo get_sub_field('news_article_image')['url']; ?>" alt="">
+								<figcaption><?php echo get_sub_field('news_article_image')['alt']; ?></figcaption>
+							</figure>
+							<?php endwhile; ?>
+						</div><!-- end of the-submenu-content-box -->
+					</div><!-- end of the-submenu-page-content -->
+				</div><!-- end of the-content -->
+			</div><!-- end of barbacco-section-content -->
+		</div><!-- end of row -->
+	</div><!-- end of container -->
+</div><!-- end of barbacco-sec-content -->
+<div class="clear"></div><!-- end of clear -->
+<?php get_footer();
